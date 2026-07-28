0154253963b3fa46b76e324c9e677704b4b4e58a
kluczi <bartekcsgo04@gmail.com>
2026-07-28 23:03:06 +0200

    Model offer dimensions and observations

---
+++ dbt/models/marts/fct_offer_observations.sql
@@ -0,0 +1,13 @@
+{{ config(
+    engine='MergeTree()',
+    order_by='(offer_key, observed_at)'
+) }}
+
+SELECT
+    source_offer_id AS offer_key,
+    observed_at,
+    price_amount,
+    price_currency,
+    mileage_km,
+    scrape_run_id
+FROM {{ ref('stg_offers_observations') }}
